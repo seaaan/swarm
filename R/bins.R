76a0ac5036099ce4bc7c3a96e1c8c70c8eb74469
Sean Hughes <smhughes@uw.edu>
2016-06-03 16:16:35 -0700

    Actually calculate bins by group

--- R/bins.R
+++ R/bins.R
@@ -17,14 +17,11 @@ get_bin_ids <- function(x, y, n_bins) {
    }
 
    # create bins
-   bins <- get_bins(y, n_bins)
-
-   # map y-values to bin number
-   #y_mapped <- map_to_bin(y, bins)
+   bins <- get_bins(y, x, n_bins)
 
    # create a bin descriptor for each (x, y) point, so all of the points with the
    # same x and in the same y bin will have the same bin descriptor
-   paste(bins, x, sep = ", ")
+   paste(x, bins, sep = ", ")
 }
 
 # Creates a numeric vector of length n_bins from min(x) to max(x),
@@ -34,10 +31,10 @@ get_bin_ids <- function(x, y, n_bins) {
 # @param n_bins the number of bins (must be > 0)
 # @return a vector of length n_bins with equally-spaced elements from
 #min(x) to max(x), inclusive.
-get_bins <- function(x, n_bins) {
+get_bins <- function(x, grouping, n_bins) {
 
-   calculate <- function(x) {
-     d <- data.frame(xx = x, order = 1:length(x))
+   calculate <- function(x, grouping) {
+     d <- data.frame(xx = x, grouping = grouping, order = 1:length(x))
 
      d <- d[order(d$xx), ]
 
@@ -60,37 +57,9 @@ get_bins <- function(x, n_bins) {
      d$bin[order(d$order)]
    }
 
-   d <- data.frame(xx = x, order = 1:length(x))
+   d <- data.frame(xx = x, order = 1:length(x), grouping = grouping) %>%
+      group_by(grouping) %>%
+      mutate(n = calculate(xx, grouping))
 
-   calculate(d$xx)
-
-
-}
-
-# Returns a numeric vector mapping each element of x to a bin.
-#
-# @param x a numeric vector
-# @param bins a sorted (increasing), numeric vector from min(x) to max(x),
-# inclusive
-# @return a vector of the same length as x, with each element indicating which
-# bin the corresponding element of x belongs in. Elements go into the largest
-# bin that is less than or equal to their value.
-map_to_bin <- function(x, bins) {
-
-   #validate parameters
-#    if ( min(x, na.rm = TRUE) != min(bins) ||
-#          (max(x, na.rm = TRUE) != max(bins) && length(bins) != 1)) {
-#       stop(paste("x and bins must have the same minimum and maximum. min(x) =",
-#          min(x, na.rm = TRUE), "min(bins) =", min(bins), "max(x) =",
-#          max(x, na.rm = TRUE), "max(bins) =", max(bins, na.rm = TRUE)))
-#    }
-   if (is.unsorted(bins)) stop("bins must be sorted, but it isn't")
-
-   result <- sapply(x, FUN = function(i) ifelse(is.na(i), 0,
-      max(which(i >= bins))))
-   # if i is NA, it will be removed later in the ggplot2 code, so return value
-   # doesn't matter. max(integer(0)), which is what happens if i == NA, generates
-   # a warning, so this avoids that
-
-   result
+   d$n
 }
